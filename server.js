631111c09727974bfae66a0cfee94b824fb33654
Farida-elgarhy <fe2200329@tkh.edu.eg>
2024-12-11 18:37:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -23,8 +23,33 @@ server.post('/user/register', (req, res) => {
         else {
             return res.status(200).send("Registration successful");
         }
-    })
-})
+    });
+});
+
+//login
+server.post('/user/login', (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+
+    if (!email || !password) {
+        return res.status(400).send("Email and password are required.");
+    }
+
+    const loginquery = `SELECT * FROM USER WHERE email = '${email}' AND password = '${password}'`;
+
+    db.get(loginquery, (err, row) => {
+        if (err){
+            console.error("Database error:", err);
+            return res.status(500).send("An error occurred.");
+        }
+      
+        if (!row) {
+            return res.status(401).send("Invalid credentials");
+        } else {
+            return res.status(200).send("Login successful");
+        }
+    });
+});
 
 
 //starting server  
